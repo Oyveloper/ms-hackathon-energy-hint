2332b6005c8f0b3b34324d8bf6c141fffa181c39
Øyvind Monsen <yvind.monsen@gmail.com>
2021-09-04 17:57:48 +0200

    adding function to get all han data

---
+++ backend/han_port/han_port_data.py
@@ -0,0 +1,13 @@
+import pymongo as pymongo
+
+
+def connect_db():
+    client = pymongo.MongoClient(
+        "mongodb+srv://db_user:very_simple_password@cluster0.eu0ag.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+
+    return client["han-data"]
+
+
+def get_han_data():
+    client = connect_db()
+    return client["datapoints"].find()
