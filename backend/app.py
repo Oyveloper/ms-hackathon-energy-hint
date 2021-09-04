a26290920592e5d64d165012e1f23106812fe5f6
Eirik Lorgen Tanberg <eirik@tanberg.org>
2021-09-04 19:45:19 +0200

    Enable CORS

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 from advice.advice_generator import get_all_advice_for_device
 from routes.advice import advice
 
 app = Flask(__name__)
+CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 app.register_blueprint(advice, url_prefix="/advice")
 
 @app.route('/')
